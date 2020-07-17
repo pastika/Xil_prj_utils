bf08e6bc8d9b34c973fd8b1efa5fb46c48b5eeff
Joe Pastika <pastika@cern.ch>
2020-07-17 17:27:09 -0500

    Remove old tcl scripts

--- tcl/generate_golden.tcl
+++
@@ -1,5 +0,0 @@
-
-source ../../src/tcl/prj_build_lib.tcl
-# syntax : generate <project_name> <device_name> <board_name>
-generate trophy_zu2eg.xpr xczu2eg-sfvc784-1-e trenz.biz:te0820_2eg_1e:part0:2.0
-#test
