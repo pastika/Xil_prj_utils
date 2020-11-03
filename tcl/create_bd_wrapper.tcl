6bbfd4eca12c1ffacac9b96bf7d101840de3475b
Andre David <andre.david@cern.ch>
2020-11-03 18:35:52 +0100

    🔇 Muting also in BD creation

--- tcl/create_bd_wrapper.tcl
+++ tcl/create_bd_wrapper.tcl
@@ -8,6 +8,9 @@ set bd_file [lindex $argv 1]
 
 open_project $project
 
+set_msg_config -severity INFO -suppress
+set_msg_config -severity STATUS -suppress
+
 # Create the top level wrapper for the project
 make_wrapper -files [get_files $bd_file] -top -import
 ######################################################################
