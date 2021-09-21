cd2841c236daed919fc3e6e034218611d79fbfb5
Jon Wilson <jsw@fnal.gov>
2021-09-20 21:03:49 -0500

    Added uHAL XML producer for AXI BRAM CTRL module

---
+++ uHALXMLProducers/uhalXMLProducer_bram_ctrl.py
@@ -0,0 +1,17 @@
+from uhalXMLProducerBase import UHALXMLProducerBase
+
+import os
+
+top_level_node_template = '<node id="%(label)s"	    mode="block"	          address="%(addr)s" size="%(size)s" permission="rw"/>'
+
+# Class MUST be named UHALXMLProducer
+class UHALXMLProducer(UHALXMLProducerBase):
+    def __init__(self, factory):
+        # Class MUST have "name" attribute defined and this must match the dtbo name of the IP it is meant to manage 
+        self.name = "axi_bram_ctrl"
+
+    # CLass MUST define produce_impl which will produce the xml map file(s) and return the top level node for the module 
+    def produce_impl(self, fragment, xmlDir, address, label):
+        size = self.getProperty(fragment, 'reg', 4)
+        
+        return top_level_node_template%{"label": label, "addr": address, "size": size}
